dabfa678e58234605d12f205bf692ac74c64b462
saldermorth <berg_gustav@hotmail.com>
2022-04-07 11:41:34 +0200

    Customer done

---
+++ Inlämningsuppgift_Dataåtkomster/Models/Enities/Customer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inlämningsuppgift_Dataåtkomster.Models.Enities
+{
+
+    [Index(nameof(Email), IsUnique = true)]
+    
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; } = null!;
+
+        [Required]        
+        [Column(TypeName = "nvarchar(250)")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public int AddressId { get; set; }
+    }
+}
