882fb85540462f73661b4129a1215c8add46e93d
saldermorth <berg_gustav@hotmail.com>
2022-04-07 09:33:47 +0200

    Add project files.

---
+++ Inlämningsuppgift_Dataåtkomster/Models/Enities/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inlämningsuppgift_Dataåtkomster.Models.Enities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
